9f4ce6aa59c69f647df2fe803e24e497a992caae
kadoshita <kadoshita.inct@gmail.com>
2017-09-08 12:42:59 +0900

    ログインユーザー数カウント

--- mackerel-windows-loginusers/Program.cs
+++ mackerel-windows-loginusers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace mackerel_windows_loginusers
 {
@@ -10,7 +11,52 @@ namespace mackerel_windows_loginusers
     {
         static void Main(string[] args)
         {
-            
+            //Processオブジェクトを作成
+            Process p = new Process();
+
+            //ComSpec(cmd.exe)のパスを取得して、FileNameプロパティに指定
+            p.StartInfo.FileName = Environment.GetEnvironmentVariable("ComSpec");
+            //出力を読み取れるようにする
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = false;
+            //ウィンドウを表示しないようにする
+            p.StartInfo.CreateNoWindow = true;
+            //コマンドラインを指定（"/c"は実行後閉じるために必要）
+            p.StartInfo.Arguments = @"/c query user";
+
+            //起動
+            p.Start();
+
+            //出力を読み取る
+            string results = p.StandardOutput.ReadToEnd();
+
+            //プロセス終了まで待機する
+            //WaitForExitはReadToEndの後である必要がある
+            //(親プロセス、子プロセスでブロック防止のため)
+            p.WaitForExit();
+            p.Close();
+
+            int loginUsers = 0;
+
+            String[] users = results.Split('\n');
+
+            for(int i = 2; i < users.Length; i++)
+            {
+                String user=users[i];
+                if (user.IndexOf("Active") != -1)
+                {
+                    loginUsers++;
+                }
+            }
+
+            Int64 retval = 0;
+            var st = new DateTime(1970, 1, 1);
+            TimeSpan t = (DateTime.Now.ToUniversalTime() - st);
+            retval = (Int64)(t.TotalMilliseconds + 0.5);
+
+            //出力された結果を表示
+            Console.WriteLine("login.user\t"+loginUsers.ToString()+"\t"+ retval.ToString());
         }
     }
 }
